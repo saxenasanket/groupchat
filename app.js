33e98c04fd69ea2ca37356470e23c867903ee2d5
Pankaj Jaiswal <pkj0017@gmail.com>
2018-01-02 18:55:43 +0530

    pankaj

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname));
 
 
-// PASSPORT CONFIGURATION
 
 app.get("/", function(req, res){
     res.render("home");
@@ -19,5 +18,5 @@ app.get("/", function(req, res){
 
 //INDEX - show all campgrounds
 app.listen(2000, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("The GC Server Has Started!");
 });
